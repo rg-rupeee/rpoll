7360635f59044af10dbcb809709402c561c2935c
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2021-09-07 00:25:03 +0530

    backend routes

--- controller/pollController.js
+++ controller/pollController.js
@@ -3,89 +3,89 @@ const { v4: uuid } = require("uuid");
 const { update } = require("./../model/pollModel");
 
 exports.createPoll = async (req, res) => {
-  try {
-    const nullArr = [];
-    for (let i = 0; i < req.body.options.length; i++) {
-      nullArr.push(0);
-    }
-    let polldata = {
-      question: req.body.question,
-      noOptions: req.body.options.length,
-      options: req.body.options,
-      voteCnt: 0,
-      votes: nullArr,
-      creater: req.body.creater,
-      uuid: uuid(),
-    };
-    const poll = await Poll.create(polldata);
-    return res.json({
-      status: "success",
-      data: poll,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.json({
-      status: "fail",
-      data: err,
-    });
-  }
+	try {
+		const nullArr = [];
+		for (let i = 0; i < req.body.options.length; i++) {
+			nullArr.push(0);
+		}
+		let polldata = {
+			question: req.body.question,
+			noOptions: req.body.options.length,
+			options: req.body.options,
+			voteCnt: 0,
+			votes: nullArr,
+			creater: req.body.creater,
+			uuid: uuid(),
+		};
+		const poll = await Poll.create(polldata);
+		return res.json({
+			status: "success",
+			data: poll,
+		});
+	} catch (err) {
+		console.log(err);
+		return res.json({
+			status: "fail",
+			data: err,
+		});
+	}
 };
 
 exports.getPoll = async (req, res) => {
-  try {
-    // console.log(req.params.uuid);
-    const poll = await Poll.findOne({ uuid: req.params.uuid });
-    return res.json({
-      status: "success",
-      data: poll,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.json({
-      status: "fail",
-      data: err,
-    });
-  }
+	try {
+		// console.log(req.params.uuid);
+		const poll = await Poll.findOne({ uuid: req.params.uuid });
+		return res.json({
+			status: "success",
+			data: poll,
+		});
+	} catch (err) {
+		console.log(err);
+		return res.json({
+			status: "fail",
+			data: err,
+		});
+	}
 };
 
 exports.vote = async (req, res) => {
-  try {
-    const op = parseInt(req.query.vote);
-    console.log(op);
+	try {
+		const op = parseInt(req.query.vote);
+		console.log(op);
 
-    const poll = await Poll.findOne({ uuid: req.params.uuid });
+		const poll = await Poll.findOne({ uuid: req.params.uuid });
 
-    // check if op is in range
-    if (op < 0 || op >= poll.noOptions) {
-      return res.json({
-        status: "fail",
-        message: "option you have chosen is not defined",
-      });
-    }
+		// check if op is in range
+		if ((!op && op !== 0) || op < 0 || op >= poll.noOptions) {
+			return res.json({
+				status: "fail",
+				message: "option you have chosen is not defined",
+			});
+		}
 
-    poll.votes[op] = poll.votes[op] + 1;
-    poll.voteCnt = poll.voteCnt + 1;
-    // increment voteCnt and vote of op option
-    const updatedPoll = await Poll.findOneAndUpdate(
-      { uuid: poll.uuid },
-      {
-        voteCnt: poll.voteCnt,
-        votes: poll.votes,
-      },
-      { new: true }
-    );
+		poll.votes[op] = poll.votes[op] + 1;
+		poll.voteCnt = poll.voteCnt + 1;
+		// increment voteCnt and vote of op option
+		const updatedPoll = await Poll.findOneAndUpdate(
+			{ uuid: poll.uuid },
+			{
+				voteCnt: poll.voteCnt,
+				votes: poll.votes,
+			},
+			{ new: true }
+		);
 
-    console.log(poll);
+		console.log(poll);
 
-    return res.json({
-      status: "success",
-      data: updatedPoll,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.json({
-      status: "fail",
-      data: err,
-    });
-  }
+		return res.json({
+			status: "success",
+			data: updatedPoll,
+		});
+	} catch (err) {
+		console.log(err);
+		return res.json({
+			status: "fail",
+			data: err,
+		});
+	}
 };
