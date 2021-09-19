ebc6882f50f6ac9e0a211a7172c1ac1aac6c8ea3
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2021-09-19 16:55:44 +0530

    added chart

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
+// view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.use(morgan("dev"));
 
 const apiRouter = require("./routes/apiRoutes");
 app.use("/api", apiRouter);
 
 app.use("/vote/:uuid", (req, res) => {
-  
+	res.render("Poll");
+});
+
+app.use("/result/:uuid", (req, res) => {
+	res.render("Poll");
 });
 
 module.exports = app;
